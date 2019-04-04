7cb5947f01fefb802257ee8ce67e328cb8aa7f96
Jamie <jnjoroge141@gmail.com>
2019-04-04 14:03:39 +0300

    done with UI components

--- app/src/main/java/com/njoroge/jamie/swiftaid/VictimCall.java
+++ app/src/main/java/com/njoroge/jamie/swiftaid/VictimCall.java
@@ -1,13 +1,166 @@
 package com.njoroge.jamie.swiftaid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.JointType;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.SquareCap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class VictimCall extends AppCompatActivity {
+
+    TextView txt_time, txt_address,txt_distance;
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_victim_call);
+
+        //initiate view
+        txt_time = (TextView)findViewById(R.id.txt_time);
+        txt_address = (TextView)findViewById(R.id.txt_address);
+        txt_distance = (TextView)findViewById(R.id.txt_distance);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.tone);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+        if (getIntent() != null)
+        {
+            double lat = getIntent().getDoubleExtra("lat", -1.0);
+            double lng = getIntent().getDoubleExtra("lng", -1.0);
+
+            getDirection(lat, lng);
+        }
+    }
+    private void getDirection(double lat, double lng) {
+
+        String requestApi;
+        try
+        {
+            requestApi = "https://maps.googleapis.com/maps/api/directions/json?"+
+                    "mode=driving&"+
+                    "transit_routing_preference= less_driving&"+
+                    "origin="+currentPosition.latitude+","+currentPosition.longitude+"&"+
+                    "destination="+destination+"&"+
+                    "key="+getResources().getString(R.string.google_direction_Api);
+
+            Log.d("JAMIE", requestApi);//prints URL for debugging
+            mService.getPath(requestApi)
+                    .enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.body());
+                                JSONArray jsonArray = jsonObject.getJSONArray("routes");
+
+                                JSONObject route = jsonArray.getJSONObject(0);
+                                JSONObject poly = route.getJSONObject("overview_polyline");
+                                String encodedString =  poly.getString("points");
+                                polyLineList = decodePoly(encodedString);
+
+                                //adjusting the bounds
+                                if(!polyLineList.isEmpty()) {
+                                    LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                                    for (LatLng latLng : polyLineList)
+                                        builder.include(latLng);
+                                    LatLngBounds bounds = builder.build();
+                                    CameraUpdate mCameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 2);
+                                    mMap.animateCamera(mCameraUpdate);
+                                }
+
+                                polylineOptions = new PolylineOptions();
+                                polylineOptions.color(Color.GRAY);
+                                polylineOptions.width(5);
+                                polylineOptions.startCap(new SquareCap());
+                                polylineOptions.endCap(new SquareCap());
+                                polylineOptions.jointType(JointType.ROUND);
+                                polylineOptions.addAll(polyLineList);
+                                greyPolyline = mMap.addPolyline(polylineOptions);
+
+
+                                blackPolylineOptions = new PolylineOptions();
+                                blackPolylineOptions.color(Color.BLACK);
+                                blackPolylineOptions.width(5);
+                                blackPolylineOptions.startCap(new SquareCap());
+                                blackPolylineOptions.endCap(new SquareCap());
+                                blackPolylineOptions.jointType(JointType.ROUND);
+                                blackPolyline = mMap.addPolyline(blackPolylineOptions);
+
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(polyLineList.get(polyLineList.size()-1))
+                                        .title("Pick up Location"));
+
+                                //Animation
+                                ValueAnimator polyLineAnimator = ValueAnimator.ofInt(0,100);
+                                polyLineAnimator.setDuration(2000);
+                                polyLineAnimator.setInterpolator(new LinearInterpolator());
+                                polyLineAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                    @Override
+                                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                        List<LatLng> points = greyPolyline.getPoints();
+                                        int percentValue = (int)valueAnimator.getAnimatedValue();
+                                        int size = points.size();
+                                        int newPoints = (int) (size * (percentValue/100.0f));
+                                        List<LatLng> p = points.subList(0, newPoints);
+                                        blackPolyline.setPoints(p);
+                                    }
+                                });
+
+                                polyLineAnimator.start();
+
+                                ambulanceMarker = mMap.addMarker(new MarkerOptions().position(currentPosition)
+                                        .flat(true)
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ambulance)));
+
+                                handler = new Handler();
+                                index = -1;
+                                next = 1;
+                                handler.postDelayed(drawPathRunnable, 3000);
+
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Toast.makeText(welcome.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
